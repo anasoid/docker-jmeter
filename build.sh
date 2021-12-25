392c1e5be03d5a7216f659b07f4a768a8d8d55e8
anasoid <oufdou.anas@gmail.com>
2021-12-25 17:00:15 +0100

    change build

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ JMETER_VERSION=${JMETER_VERSION:-"5.4"}
 IMAGE_TIMEZONE=${IMAGE_TIMEZONE:-"Europe/Amsterdam"}
 
 # Example build line
-docker build  --build-arg JMETER_VERSION=${JMETER_VERSION} --build-arg TZ=${IMAGE_TIMEZONE} -t "justb4/jmeter:${JMETER_VERSION}" .
+docker build  --build-arg JMETER_VERSION=${JMETER_VERSION} --build-arg TZ=${IMAGE_TIMEZONE} -t "anasoid/jmeter:${JMETER_VERSION}" .
