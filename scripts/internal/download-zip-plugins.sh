9bf13c6468922058d07bf2e143b1104f5463494a
anasoid <oufdou.anas@gmail.com>
2022-01-06 21:04:58 +0100

    corrct download url

--- scripts/internal/download-zip-plugins.sh
+++ scripts/internal/download-zip-plugins.sh
@@ -10,12 +10,12 @@ if [ "$CONF_SKIP_PLUGINS_INSTALL" == "false" ]; then
    for file in ${files[@]}; do
       if [ -f "$file" ]; then
          while IFS= read -r line; do
-            echo "$line"
-            rm -f temp.zip
-            wget $file -O /tmp/temp.zip
-            unzip temp.zip
-            unzip -o temp.zip -d ${JMETER_ADDITIONAL_TARGET}
-            rm -f temp.zip
+            echo "Downloading : $line"
+            rm -f /tmp/temp.zip
+            wget $line -O /tmp/temp.zip
+            unzip /tmp/temp.zip
+            unzip -o /tmp/temp.zip -d ${JMETER_ADDITIONAL_TARGET}
+            rm -f /tmp/temp.zip
          done <"$file"
       else
          echo "Skip download zip extension plugin from : $file, not found "
