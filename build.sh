98448d7f3b7c799e06e777b300e7d3b3d5408452
anasoid <oufdou.anas@gmail.com>
2022-01-07 08:53:20 +0100

    clean

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-JMETER_VERSION=${JMETER_VERSION:-"5.4"}
-IMAGE_TIMEZONE=${IMAGE_TIMEZONE:-"Europe/Amsterdam"}
-
-# Example build line
-docker build  --build-arg JMETER_VERSION=${JMETER_VERSION} --build-arg TZ=${IMAGE_TIMEZONE} -t "anasoid/jmeter:${JMETER_VERSION}" .
